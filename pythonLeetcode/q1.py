56e49ac33d398db75c237ef2fbfac83074864c8a
benroo <benroo123@126.com>
2022-02-07 06:19:21 -0500

    question1

---
+++ pythonLeetcode/q1.py
@@ -0,0 +1,22 @@
+# two sum
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         pass
+
+
+def twoSum(nums, target):
+    result = [0, 0]
+    for i in range(len(nums) - 1):
+        for j in range(i, len(nums)):
+            print(i, j)
+            if nums[i] + nums[j] == target:
+                result[0], result[-1] = i, j
+
+    return result
+
+
+print(twoSum([-5, 11, 15, -1], -6))
